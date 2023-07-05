aa288329f32bc7033b3da9c9f0c1626db8f7f562
NVT-Game <toainguyenprogramminggame@gmail.com>
2023-07-05 23:44:40 +0700

    trang gioi thieu

--- main.js
+++ main.js
@@ -2,25 +2,25 @@
 var prevScrollPos = window.pageYOffset;
 addEventListener("wheel", (event) => { });
 
-onwheel = (event) => {
-    var currentScrollPos = window.pageYOffset;
-    var scrollPosition = window.scrollY;
+// onwheel = (event) => {
+//     var currentScrollPos = window.pageYOffset;
+//     var scrollPosition = window.scrollY;
 
-    const headerBrand = document.getElementById("header-brand");
-    const navbar = document.getElementById("wrap-navbar");
+//     const headerBrand = document.getElementById("header-brand");
+//     const navbar = document.getElementById("wrap-navbar");
     
-    if (prevScrollPos > currentScrollPos) {
-        // Cuộn lên
-        if (scrollPosition < 150) {
-            headerBrand.style.display = "block";
-            navbar.style.position = "relative";
-        }
-    } else {
-        // Cuộn xuống
-        if (scrollPosition > 40) {
-            headerBrand.style.display = "none";
-            navbar.style.position = "fixed";
-        }
-    }
-    prevScrollPos = currentScrollPos;
-};
+//     if (prevScrollPos > currentScrollPos) {
+//         // Cuộn lên
+//         if (scrollPosition < 150) {
+//             headerBrand.style.display = "block";
+//             navbar.style.position = "relative";
+//         }
+//     } else {
+//         // Cuộn xuống
+//         if (scrollPosition > 40) {
+//             headerBrand.style.display = "none";
+//             navbar.style.position = "fixed";
+//         }
+//     }
+//     prevScrollPos = currentScrollPos;
+// };
